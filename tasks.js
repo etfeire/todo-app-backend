e57df3ef089accf98ac450ef4091abfca8eacd39
etfeire <etfeire@gmail.com>
2019-05-29 20:39:54 +0100

    amend tasks.js file

--- tasks.js
+++ tasks.js
@@ -8,9 +8,15 @@ app.get('/tasks', function (request, response) {
 
   const username = request.query.username;
 
-  const someJson = {
-    message: "Hello " + username + ", how are you? You have the following tasks to complete today: "
-  };
+  const todoList = request.query.tasks;
+
+  // const someJson = {
+  //   message: "Hello " + username + ", how are you? You have the following tasks to complete today: "
+  // };
+  // const someJson = {
+  //   tasks: "Hello " + username + ", here are your tasks: " + tasks;
+  // }
+  const someJson = [tasks];
   response.json(someJson);
 })
 
