a5aaa8522580ebb6d95b9aea36a1946e32e56c74
etfeire <etfeire@gmail.com>
2019-05-30 18:00:52 +0100

    modified tasks.js in effort to return tasks

--- tasks.js
+++ tasks.js
@@ -6,17 +6,28 @@ app.use(cors());
 
 app.get('/tasks', function (request, response) {
 
-  // const username = request.query.username;
+  const username = request.query.username;
 
-  const todoList = request.query.tasks;
+  // const todoList = request.query.tasks;
 
+  const tasks = [
+    {task: "Attend TechReturners course", completed: true, id: uuid()}, 
+    {task: "Practise!", completed: false, id: uuid()}, 
+    {task:"Do your homework", completed:false, id: uuid()}, 
+    {task:"Build final project", completed: true, id: uuid()}, 
+    {task:"Drink coffee", completed: false, id: uuid()},
+    {task: "Apply to tech companies", completed: false, id: uuid()}
+  ];
+
+  const someJson = {
+    message: "Hello " + username + ", here are your tasks: " + tasks;
+  };
   // const someJson = {
   //   message: "Hello " + username + ", how are you? You have the following tasks to complete today: "
   // };
   // const someJson = {
   //   tasks: "Hello " + username + ", here are your tasks: " + tasks;
   // }
-  const someJson = [todoList];
   response.json(someJson);
 })
 
