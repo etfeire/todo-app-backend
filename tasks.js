6c4de3d87ae7178e8a7b482f4b3734e1d3cd7c97
etfeire <etfeire@gmail.com>
2019-05-30 19:10:49 +0100

    modified tasks

--- tasks.js
+++ tasks.js
@@ -6,7 +6,7 @@ app.use(cors());
 
 app.get('/tasks', function (request, response) {
 
-  const username = request.query.username;
+  // const username = request.query.username;
 
   // const todoList = request.query.tasks;
 
@@ -20,13 +20,13 @@ app.get('/tasks', function (request, response) {
   // ];
 
   const someJson = {
-    tasks = [
-      {task: "Attend TechReturners course", completed: true, id: uuid()}, 
-      {task: "Practise!", completed: false, id: uuid()}, 
-      {task:"Do your homework", completed:false, id: uuid()}, 
-      {task:"Build final project", completed: true, id: uuid()}, 
-      {task:"Drink coffee", completed: false, id: uuid()},
-      {task: "Apply to tech companies", completed: false, id: uuid()}
+    tasks: [
+      {task: "Attend TechReturners course", completed: true, id: 1}, 
+      {task: "Practise!", completed: false, id: 2}, 
+      {task:"Do your homework", completed:false, id: 3}, 
+      {task:"Build final project", completed: true, id: 4}, 
+      {task:"Drink coffee", completed: false, id: 5},
+      {task: "Apply to tech companies", completed: false, id: 6}
     ]
   };
   // const someJson = {
